cd259e003fb40272c5b652019a43ac08bfd9f097
Alessandro292 <alessandro.impagnati@globalservs.com>
2023-11-05 10:37:20 +0100

    testing hello routes

--- tests/test_hello_api.py
+++ tests/test_hello_api.py
@@ -5,3 +5,16 @@ def test_hello_world_endpoint(client):
     response = client.get(f'{API_PREFIX}/')
     assert response.status_code == 200
     assert b"Hello, World!" in response.data
+
+def test_hello_route_without_name(client):
+    response = client.get(f'{API_PREFIX}/hello/')
+    assert response.status_code == 200
+    assert b'Hello World, from Flask' in response.data
+
+def test_hello_route_with_name(client):
+    response = client.get(f'{API_PREFIX}/hello/John')
+    assert response.status_code == 200
+    assert b'Hello John, from Flask' in response.data
+
+
+
